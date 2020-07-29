2d23c9b87320fd009840ba2b5d686ccb376c5939
Jon McLachlan <jon.mclachlan@icloud.com>
2020-07-28 22:58:36 -0700

    Towards a demo

--- example-android/app/src/main/java/com/example/helloworld/SecondFragment.java
+++ example-android/app/src/main/java/com/example/helloworld/SecondFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -22,6 +23,10 @@ public class SecondFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        TextView myAwesomeTextView = (TextView)view.findViewById(R.id.textView);
+
+        myAwesomeTextView.setText("Well... encrypted thing goes here.");
 
         view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override
