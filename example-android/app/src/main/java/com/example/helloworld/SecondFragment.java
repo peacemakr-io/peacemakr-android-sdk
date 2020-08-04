3e6c3526e376ecfcad6cf6ae33dc6b2e33b14695
Jon McLachlan <jon.mclachlan@icloud.com>
2020-08-03 21:23:53 -0700

    This is a nice working android app. Yey :)

--- example-android/app/src/main/java/com/example/helloworld/SecondFragment.java
+++ example-android/app/src/main/java/com/example/helloworld/SecondFragment.java
@@ -1,6 +1,7 @@
 package com.example.helloworld;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,31 +28,42 @@ public class SecondFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
 
+    static ICrypto CryptoI = null;
+
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        String apiKey = "your-api-key";
+
+
+        // The main thread is not usually permitted to make
+        // network calls, but for the sake of a quick example,
+        // we're removing that restriction.
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        String apiKey = "PUT YOUR API KEY HERE :)";
         InMemoryPersister persister = new InMemoryPersister();
 
-        ICrypto cryptoI = null;
-        try {
-            cryptoI = Factory.getCryptoSDK(apiKey, "simple encrypt decrypt", null, persister, null);
-        } catch (PeacemakrException e) {
-            Log.e("peacemakr", "Failed to construct crypt sdk due to ", e);
-            return;
+        if (CryptoI == null) {
+            try {
+                CryptoI = Factory.getCryptoSDK(apiKey, "simple encrypt decrypt", null, persister, null);
+            } catch (PeacemakrException e) {
+                Log.e("peacemakr", "Failed to construct crypt sdk due to ", e);
+                return;
+            }
+            try {
+                CryptoI.register();
+            } catch (PeacemakrException e) {
+                Log.e("peacemakr", "Failed to register crypt sdk due to ", e);
+                return;
+            }
         }
-        try {
-            cryptoI.register();
-        } catch (PeacemakrException e) {
-            Log.e("peacemakr", "Failed to register crypt sdk due to ", e);
-            return;
-        }
+
 
         String plaintext = "Hello world!";
-
         byte[] encrypted = new byte[0];
         try {
-            encrypted = cryptoI.encrypt(plaintext.getBytes());
+            encrypted = CryptoI.encrypt(plaintext.getBytes());
         } catch (PeacemakrException e) {
             Log.e("peacemkar", "Failed to encrypt due to ", e);
             return;
@@ -59,13 +71,11 @@ public class SecondFragment extends Fragment {
         TextView myAwesomeTextView = view.findViewById(R.id.textView);
         myAwesomeTextView.setText("Encrypted: " + new String(encrypted));
         System.out.println("Encrypted: " + new String(encrypted));
-
-
         System.out.println();
 
         byte[] decrypted = new byte[0];
         try {
-            decrypted = cryptoI.decrypt(encrypted);
+            decrypted = CryptoI.decrypt(encrypted);
         } catch (PeacemakrException e) {
             Log.e("peacemakr", "Failed to decrypt due to ", e);
         }
